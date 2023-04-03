5260277585ed808117a93555ffc4f90fae839489
muhammadbasimkhalid <101529055+muhammadbasimkhalid@users.noreply.github.com>
2023-04-04 03:30:55 +0500

    changes done

--- zakatamount.py
+++ zakatamount.py
@@ -1,33 +1,33 @@
-q=input("what do you have?gold\silver\cash:")
-b=input("you have this amount for one year? Yes or No:")
+item=input("what do you have?gold\silver\cash:")
+time=input("you have this amount for one year? Yes or No:")
 
 def gold():
-    a=float(input("how much gold do you have?"))
-    if a>=7.5 and b=="Yes":
-        c=a*196400*0.025
+    amount=float(input("how much gold do you have?"))
+    if amount>=7.5 and time=="Yes":                                                        #a=amnt b= time Q=item
+        c=amount*196400*0.025
         print("you have to pay",c)
     else:
             print("you are not eligible for paying zakat")
 def silver():
-    s=float(input("how much silver do you have?"))
-    if s>=52.5and b=="Yes":
-        m=s*2082*0.025
+    amount1=float(input("how much silver do you have?"))
+    if amount1>=52.5 and time=="Yes":
+        m=amount1*2082*0.025
         print("you have to pay",m)
     else:
-           print("you are not eligible for paying zakat")
+           print("you are not eligible for paying zakat")a
 def cash():
-    d=float(input("how much cash do you have"))
-    if d>=109305 and b=="Yes":
-        g=d*0.025
+    amount2=float(input("how much cash do you have"))
+    if amount2>=109305 and time=="Yes":
+        g=amount2*0.025
         print("you have to pay",g)
     else:
         print("you are not eligible for paying zakat")
     
-if q=="gold":
+if item=="gold":
     gold()
-elif q=="silver":
+elif item=="silver":
     silver()
-elif q=="cash":
+elif item=="cash":
     cash()
 else:
     print("invalid input")
